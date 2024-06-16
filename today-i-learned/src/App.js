8f738da33ab974df2a64fcf79e1d5859631699e2
Chirag Solanki <solankichiragcs12@gmail.com>
2024-06-16 20:19:52 +0530

    app.js file modified

--- today-i-learned/src/App.js
+++ today-i-learned/src/App.js
@@ -1,14 +1,40 @@
 import "./style.css";
 function App() {
+  const appTitle = "Today I Learned";
   return (
-    <header className="header">
-      <div className="logo">
-        <img src="logo.png" height="68" width="68" alt="Today I Learned Logo" />
-        <h1>Today I Learned</h1>
-      </div>
-      <button className="btn btn-large btn-open">Share a fact</button>
-    </header>
+    <>
+      {/* HEADER */}
+      <header className="header">
+        <div className="logo">
+          <img
+            src="logo.png"
+            height="68"
+            width="68"
+            alt="Today I Learned Logo"
+          />
+          <h1>{appTitle}</h1>
+        </div>
+        <button className="btn btn-large btn-open">Share a fact</button>
+      </header>
+      <NewFactForm />
+      <main className="main">
+        <CategoryFilter />
+        <FactList />
+      </main>
+    </>
   );
+}
+
+function NewFactForm() {
+  return <form className="fact-form">Fact form</form>;
+}
+
+function CategoryFilter() {
+  return <aside>Category Filter</aside>;
+}
+
+function FactList() {
+  return <section>Facts List</section>;
 }
 
 export default App;
